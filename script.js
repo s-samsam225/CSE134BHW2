a0b27ab5b5a49310db0c5985b03edd60e491ab02
Sameera Agarwala <sameera.agarwala@gmail.com>
2025-10-21 22:53:42 -0700

    Added project subpage

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+class PortfolioCallout extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+
+customElements.define("portfolio-callout", PortfolioCallout);
+
+
 class HelloWorld extends HTMLElement {
     connectedCallback() {
       console.log("Hello world! — from <hello-world>");
